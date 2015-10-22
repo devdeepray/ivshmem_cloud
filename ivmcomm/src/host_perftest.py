eb99f5877f3c3913d847e20f6f25e500411fd5d2
Devdeep Ray <devdeepray@gmail.com>
2015-10-22 13:30:39 +0530

    Added perf tester

---
+++ ivmcomm/src/host_perftest.py
@@ -0,0 +1,23 @@
+from xmlrpc.client import ServerProxy
+from time import time
+from threading import Thread
+
+NUM_THREADS = 1
+NUM_REQ_PER_THREAD = 10000
+
+
+def reqGen(tid):
+	s = ServerProxy('http://127.0.0.1:8888')
+	start = time()
+	for i in range(0, NUM_REQ_PER_THREAD):
+		s.openShm(i % 512, tid, tid, 'w', False)
+		s.closeShm((i + 256) % 512, tid, tid)
+	end = time()
+	print ('rate = ' + str(NUM_REQ_PER_THREAD / (end - start)));
+
+
+for i in range(0, NUM_THREADS):
+	t = Thread(target=reqGen, args=(i, ))
+	t.start()
+
+
